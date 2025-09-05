e2578986a715d71d10165d4e17fb2991970bb0ac
cheeseball <cheeseballbusinessemail@gmail.com>
2025-09-05 15:53:05 +0800

    Factorial (ez didn't even have to search)

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        f.solve();
+    }
+
+    private void solve() {
+        Scanner userInput = new Scanner(System.in);
+
+        int number = userInput.nextInt();
+        int factorial = 1;
+
+        for(int i = number; i > 0; i--) {
+            factorial *= i;
+        }
+
+        System.out.println(factorial);
+    }
+}
