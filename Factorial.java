2cc80bc736154a186029b0024b5ca05431d5aa2e
cheeseball <cheeseballbusinessemail@gmail.com>
2025-09-05 16:12:54 +0800

    Corrected java factorial

--- Factorial.java
+++ Factorial.java
@@ -12,11 +12,9 @@ public class Factorial {
 
         int number = userInput.nextInt();
         int factorial = 1;
-
-        for(int i = number; i > 0; i--) {
+        for(int i = 1; i <= number; i++) {
             factorial *= i;
         }
-
         System.out.println(factorial);
     }
 }
