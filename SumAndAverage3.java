1b2deba786c7a7566a0ecfaeda6972cac72fe4d5
cheeseball <cheeseballbusinessemail@gmail.com>
2025-09-11 00:00:56 +0800

    Practiced sum average

---
+++ SumAndAverage3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SumAndAverage3 {
+    public static void main(String[] args) {
+        SumAndAverage3 sa = new SumAndAverage3();
+        sa.solve();
+    }
+
+    private void solve() {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        String number = userInput.nextLine();
+        int sum = 0;
+        int count = 0;
+        int avg = 0;
+        for(int i = 0; i < number.length(); i++) {
+            sum += Character.getNumericValue(number.charAt(i));
+            count++;
+        }
+
+        avg = sum / count;
+
+        System.out.println("The sum of all digits: " + sum);
+        System.out.println("The average: " + avg);
+
+    }
+}
