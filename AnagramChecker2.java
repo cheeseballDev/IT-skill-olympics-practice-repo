db5dc0a473eba80d13790e421ecb74caaa503001
cheeseball <cheeseballbusinessemail@gmail.com>
2025-09-11 22:36:00 +0800

    Added anagram checker

---
+++ AnagramChecker2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AnagramChecker2 {
+    public static void main(String[] args) {
+        AnagramChecker2 ac = new AnagramChecker2();
+        System.out.print(ac.solve());
+    }
+
+    private String solve() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter first word: ");
+        String word1 = userInput.nextLine();
+        System.out.print("Enter the second word: ");
+        String word2 = userInput.nextLine();
+
+
+        int[] alphabets = new int[26];
+
+        for(char c : word1.toCharArray()) {
+            alphabets[c - 'a']++;
+        }
+
+        for(char c : word2.toCharArray()) {
+            alphabets[c - 'a']--;
+        }
+
+        for(int i = 0; i < alphabets.length; i++) {
+            if(alphabets[i] == 1) {
+                return "is NOT anagram!";
+            }
+        }
+        return "Is anagram!";
+    }
+}
