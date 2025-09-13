1554d9da296acf03c3aba62aa607a735b880f956
cheeseball <cheeseballbusinessemail@gmail.com>
2025-09-14 00:56:16 +0800

    Practiced jagged arrays

---
+++ JaggedArray7.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class JaggedArray7 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter the amount of rows: ");
+        int row = userInput.nextInt();
+        int[][] array = new int[row][];
+        
+        for(int i = 0; i < row; i++) {
+            System.out.printf("Enter the amount of columns for row %d: ", i + 1);
+            array[i] = new int[userInput.nextInt()]; 
+        }
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < array[i].length; j++) {
+                System.out.printf("Enter element at position (%d, %d): ", i, j);
+                array[i][j] = userInput.nextInt();
+            }
+        }
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
