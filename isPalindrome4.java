3568258ca5199309cb65ccbdf2551e6baaa39ae7
cheeseball <cheeseballbusinessemail@gmail.com>
2025-09-16 00:11:50 +0800

    Palindrome

---
+++ isPalindrome4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class isPalindrome4 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter first word: ");
+        String word = userInput.nextLine();
+
+        int wordLength = word.length();
+
+        String reversedWord = "";
+        for(int i = wordLength - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+
+        String condition = word.equals(reversedWord) ? "Is palindrome" : "Not";
+
+        System.out.println(condition);
+
+    }
+}
