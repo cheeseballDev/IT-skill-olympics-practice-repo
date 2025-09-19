58d3c40be33071303065fd5f6f191802173e2d64
cheeseball <cheeseballbusinessemail@gmail.com>
2025-09-19 22:59:01 +0800

    Rotate a matrix

--- ReverseNumberNoString4.java
+++ ReverseNumberNoString4.java
@@ -8,8 +8,11 @@ public class ReverseNumberNoString4 {
 
         int reversed = 0;
         while(number >= 1) {
+            // gets the last digit of the number
             int lastDigit = number % 10;
+            // adds the last digit first added by the reversed number and assigns it to the reversed number multiplied by 10 to increment it
             reversed = lastDigit + reversed * 10;
+            // number is removed by 1 place
             number = number / 10;
         }
         System.out.println(reversed);
---
+++ RotateAMatrix5.java
@@ -0,0 +1,27 @@
+public class RotateAMatrix5 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        int matrixRow = matrix.length;
+        int matrixCol = matrix[0].length;
+
+        int[][] tempMatrix = new int[matrixRow][matrixCol];
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                tempMatrix[j][matrixRow - 1 - i] = matrix[i][j];
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                System.out.print(tempMatrix[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
