718ca520ff747b1d4f10ddae62a918d3cfef909c
cheeseball <cheeseballbusinessemail@gmail.com>
2025-09-19 22:53:38 +0800

    Reversed

---
+++ ReverseNumberNoString4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseNumberNoString4 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        int number = userInput.nextInt();
+
+        int reversed = 0;
+        while(number >= 1) {
+            int lastDigit = number % 10;
+            reversed = lastDigit + reversed * 10;
+            number = number / 10;
+        }
+        System.out.println(reversed);
+    }
+}
